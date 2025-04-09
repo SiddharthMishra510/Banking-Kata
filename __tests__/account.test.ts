2d92648bc9ceb9a855761dd5fa9d72d4d94be4ef
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-04-10 04:04:01 +0530

    feat: maintain array of transactions and print sum of transactions while printing statement

--- __tests__/account.test.ts
+++ __tests__/account.test.ts
@@ -32,4 +32,16 @@ describe('Account', () => {
     expect(account.printStatement()).toBe('10.4.2025 | +5 | 5');
     jest.useRealTimers();
   });
+
+  it('should print correct statement after two deposit of 1 unit each on a given date', () => {
+    const account = new Account();
+    const mockDate = new Date('4.10.2025');
+    jest.useFakeTimers().setSystemTime(mockDate);
+    account.deposit(1);
+    account.deposit(1);
+    expect(account.printStatement()).toBe(
+      '10.4.2025 | +1 | 1\n10.4.2025 | +1 | 2'
+    );
+    jest.useRealTimers();
+  });
 });
--- src/account.ts
+++ src/account.ts
@@ -1,19 +1,27 @@
 import { Transaction } from './transaction';
 
 export class Account {
-  private transaction!: Transaction;
+  private transactions: Transaction[] = new Array<Transaction>();
 
   deposit(number: number) {
     if (number < 0) {
       throw new Error('Deposit must be greater than 0');
     }
-    this.transaction = new Transaction(
-      number,
-      new Intl.DateTimeFormat('de-DE').format(new Date())
+    this.transactions.push(
+      new Transaction(
+        number,
+        new Intl.DateTimeFormat('de-DE').format(new Date())
+      )
     );
   }
 
   printStatement(): string {
-    return `${this.transaction.date} | +${this.transaction.amount} | ${this.transaction.amount}`;
+    let amount = 0;
+    const lines = this.transactions.map((transaction) => {
+      amount += transaction.amount;
+      return `${transaction.date} | +${transaction.amount} | ${amount}`;
+    });
+
+    return lines.join('\n');
   }
 }
